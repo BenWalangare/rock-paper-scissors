34be7ffc9d3c71b833a3f7cba4a85b465cd3578e
BenWalls <ruben.walangare@gmail.com>
2021-12-10 00:30:15 +0700

    Create working scoring

--- script.js
+++ script.js
@@ -12,6 +12,15 @@ function computerPlay() {
     }
 }
 
+function gameFinish() {
+    if (playerPoint === 5) {
+        return alert("Player Win");
+    };
+    if (compPoint === 5) {
+        return alert("Computer Win");
+    };
+}
+
 // step 2: we create the rule of the game
 // this function dictates the rule of the game
 function roundResult(playerSelection, computerSelection) {
@@ -19,28 +28,38 @@ function roundResult(playerSelection, computerSelection) {
         alert("Tie! Rock");
     } else if (playerSelection === "rock" && computerSelection === "Paper") {
         alert("You Lose! Paper beats Rock");
-        return compPoint += 1;
+        compPoint += 1;
+        gameFinish();
+        console.log(playerPoint, compPoint);
     } else if (playerSelection === "rock" && computerSelection === "Scissors") {
         alert("You Win! Rock crushes Scissors");
-        return playerPoint += 1
+        playerPoint += 1;
+        gameFinish();
+        console.log(playerPoint, compPoint)
     } else if (playerSelection === "paper" && computerSelection === "Rock") {
         alert("You Win! Paper suffocates Rock");
-        return playerPoint += 1;
+        playerPoint += 1;
+        gameFinish();
+        console.log(playerPoint, compPoint)
     } else if (playerSelection === "paper" && computerSelection === "Paper") {
         alert("Tie! Paper");
     } else if (playerSelection === "paper" && computerSelection === "Scissors") {
         alert("You Lose! Scissors cut Paper");
-        return compPoint += 1;
+        compPoint += 1;
+        gameFinish();
+        console.log(playerPoint, compPoint);
     } else if (playerSelection === "scissors" && computerSelection === "Rock") {
         alert("You lose! Rock crushes Scissors")
-        return compPoint += 1;
+        compPoint += 1;
+        gameFinish();
+        console.log(playerPoint, compPoint);
     } else if (playerSelection === "scissors" && computerSelection === "Paper") {
         alert("You Win! Scissors cut Paper")
-        return playerPoint += 1;
+        playerPoint += 1;
+        gameFinish();
+        console.log(playerPoint, compPoint)
     } else if (playerSelection === "scissors" && computerSelection === "Scissors") {
         alert("Tie! Scissors");
-    } else {
-        alert("Please type either Rock, Paper, or Scissors, it's case insensitive! (:")
     }
 }
 
@@ -48,24 +67,39 @@ function roundResult(playerSelection, computerSelection) {
 let playerPoint = 0;
 let compPoint = 0;
 
-/*
+
+const choices = document.querySelectorAll("buttons")
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
 // step 3: we are looping the game and put the score outside the loop up until the player or comp meet the first 5 and it breaks the loop
-for (;;) { // this is the proper code to create infinite loop 
-    const playerSelection = prompt("Rock, Paper, or Scissors? \n" + "The current score for Player = " +
-                            playerPoint + " and for Computer = " + compPoint).toLowerCase();
-    console.log(playerSelection);
-    const computerSelection = computerPlay();
-    roundResult(playerSelection, computerSelection);
-    console.log(playerPoint);
-    console.log(compPoint);
-    // below are if condition, it calculates the variable and will break the loops if it meets its condition
-    if (playerPoint === 5) {
-        alert("Player Win");
-        break;
-    }
-    if (compPoint === 5) {
-        alert("Computer Win");
-        break;
-    }
-}
+// this is the proper code to create infinite loop 
+
+if (rock.addEventListener("click", () => {
+    console.log(playerSelection = "rock");
+    computerSelection = computerPlay();
+    console.log(roundResult(playerSelection, computerSelection));
+}));
+
+if (paper.addEventListener("click", () => {
+    console.log(playerSelection = "paper");
+    computerSelection = computerPlay();
+    console.log(roundResult(playerSelection, computerSelection));
+}));
+
+if (scissors.addEventListener("click", () => {
+    console.log(playerSelection = "scissors");
+    computerSelection = computerPlay();
+    console.log(roundResult(playerSelection, computerSelection));
+}));
+
+
+// below are if condition, it calculates the variable and will break the loops if it meets its condition
+
+/*
+const playerSelection = prompt("Rock, Paper, or Scissors? \n" + "The current score for Player = " +
+playerPoint + " and for Computer = " + compPoint).toLowerCase();
+console.log(playerSelection);
 */
